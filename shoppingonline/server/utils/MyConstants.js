b7cc6cce1c04c56154d93d8af4de37fa72699baa
QuanDinh157 <lachongquan12@gmail.com>
2026-04-06 14:37:29 +0700

    Add files via upload

---
+++ shoppingonline/server/utils/MyConstants.js
@@ -0,0 +1,14 @@
+const MyConstants = {
+  DB_SERVER: "cluster0.dbwymd7.mongodb.net",
+  DB_USER: "kha",
+  DB_PASS: "123",
+  DB_DATABASE: "shoppingonline_vlu",
+
+  EMAIL_USER: "dingzcraft157@gmail.com",
+  EMAIL_PASS: "kctfajqtwqqyozqy",
+
+  JWT_SECRET: "abciooiqwe",
+  JWT_EXPIRES: "356658435644111",
+};
+
+module.exports = MyConstants;
